126124f9bc4170912e50e82715b02b1e6151bbbc
C62848A <Vo.ThuyLinh@experian.com>
2020-10-01 23:55:28 +0800

    PassTransfer: Init Project

---
+++ passtransfer/src/main/java/com/linhrepo/spring/passtransfer/persistent/UserEntityRepository.java
@@ -0,0 +1,13 @@
+package com.linhrepo.spring.passtransfer.persistent;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * 
+ * @author linhvo
+ *
+ * https://github.com/linhvovn/medium
+ */
+public interface UserEntityRepository extends CrudRepository<UserEntity, Integer>{
+
+}
